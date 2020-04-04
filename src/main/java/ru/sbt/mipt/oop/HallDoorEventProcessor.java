408949623a056808ddb1459c3e76ffa5f411dbf9
Keel-Ranuir <volha.rednayaa@gmail.com>
2020-04-04 21:47:14 +0300

    hw 1 added

---
+++ src/main/java/ru/sbt/mipt/oop/HallDoorEventProcessor.java
@@ -0,0 +1,34 @@
+package ru.sbt.mipt.oop;
+
+import static ru.sbt.mipt.oop.SensorEventType.DOOR_CLOSED;
+
+public class HallDoorEventProcessor implements EventProcessor {
+
+    @Override
+    public void processEvent(SmartHome smartHome, SensorEvent event) {
+
+        if (event.getType() != DOOR_CLOSED) {
+            return;
+        }
+
+        for (Room room : smartHome.getRooms()) {
+            for (Door door : room.getDoors()) {
+                if (door.getId().equals(event.getObjectId())) {
+                    if (room.getName().equals("hall")) {
+                        turnOffAllLights(smartHome);
+                    }
+                }
+            }
+        }
+    }
+
+    private static void turnOffAllLights(SmartHome smartHome) {
+        for (Room room : smartHome.getRooms()) {
+            for (Light light : room.getLights()) {
+                light.setOn(false);
+                SensorCommand command = new SensorCommand(CommandType.LIGHT_OFF, light.getId());
+                CommandSender.sendCommand(command);
+            }
+        }
+    }
+}
